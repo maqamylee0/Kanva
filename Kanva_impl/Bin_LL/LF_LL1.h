31f1651cec2199df688d80253e72ae1880d7628a
gaurav-bhardwaj03 <cs19resch11003@iith.ac.in>
2022-08-15 23:22:02 +0530

    Kanva

---
+++ Kanva_impl/Bin_LL/LF_LL1.h
@@ -0,0 +1,132 @@
+
+
+#ifndef UNTITLED_LF_LL_H
+#define UNTITLED_LF_LL_H
+
+#include<atomic>
+#include <limits>
+#include<vector>
+#include <unordered_set>
+#include <iostream>
+#include "util1.h"
+
+
+template<typename K, typename V>
+class Linked_List {
+public:
+    ll_Node<K, V> *head;
+    std::atomic<int> count;
+    Linked_List() {
+        ll_Node<K, V> *max = new ll_Node<K, V>(std::numeric_limits<K>::max(), 0);
+        ll_Node<K, V> *min = new ll_Node<K, V>(std::numeric_limits<K>::min(), 0);
+        min->next.store(max);
+        head = min;
+    }
+
+
+    void mark();
+
+    int insert(K Key, V value);
+
+    bool insert(K Key, V value, ll_Node<K, V> *new_node);
+
+    int insert(K Key, V value, int tid, int phase);
+
+    bool search(K key);
+
+    void collect(std::vector<K> &, std::vector<V> &);
+
+    void range_query(int64_t low, int64_t high, int64_t curr_ts, std::vector<std::pair<K, V>> &res);
+
+    ll_Node<K, V> *find(K key);
+
+    ll_Node<K, V> *find(K key, ll_Node<K, V> **);
+};
+
+
+template<typename K, typename V>
+void Linked_List<K,V>::collect(std::vector<K> &keys, std::vector<V> &vals) {
+    ll_Node<K,V>* left_node = head;
+    int i = 0;
+    while(left_node -> next.load(std::memory_order_seq_cst)){
+        if(!is_marked_ref((long) left_node -> next.load(std::memory_order_seq_cst)))
+        {
+            ll_Node<K,V>* curr_next = left_node -> next.load(std::memory_order_seq_cst);
+            left_node -> next.compare_exchange_strong(curr_next, (ll_Node<K,V>*)set_mark((long) curr_next));
+        }
+//
+        left_node = (ll_Node<K,V>*)get_unmarked_ref((long) left_node -> next.load(std::memory_order_seq_cst));
+        keys.push_back(left_node-> key);
+        vals.push_back(left_node->value);
+    }
+}
+
+
+template<typename K, typename V>
+bool Linked_List<K,V>::search(K key) {
+    ll_Node<K,V>* curr = head;
+    while(curr -> key < key)
+        curr = (ll_Node<K,V>*) get_unmarked_ref((long) curr -> next.load(std::memory_order_seq_cst));
+    return (curr -> key == key && !is_marked_ref((long) curr -> next.load(std::memory_order_seq_cst)));
+}
+
+
+
+template<typename K, typename V>
+ll_Node<K,V>* Linked_List<K,V>::find(K key) {
+    ll_Node<K, V> *left_node = head;
+    while(true) {
+        ll_Node<K, V> *right_node = left_node -> next.load(std::memory_order_seq_cst);
+        if (is_marked_ref((long) right_node))
+            return nullptr;
+        if (right_node->key >= key)
+            return right_node;
+        (left_node) = right_node;
+    }
+}
+
+template<typename K, typename V>
+ll_Node<K,V>* Linked_List<K,V>::find(K key, ll_Node<K, V> **left_node) {
+    (*left_node) = head;
+    while(true) {
+        ll_Node<K, V> *right_node = (*left_node) -> next.load(std::memory_order_seq_cst);
+        if (is_marked_ref((long) right_node)) {
+            return nullptr;
+        }
+        if (right_node -> key >= key)
+            return right_node;
+        (*left_node) = right_node;
+    }
+}
+
+template<typename K, typename V>
+int Linked_List<K,V>::insert(K key, V value) {
+//    return -1;
+    while(true)
+    {
+        ll_Node<K,V>* prev_node = nullptr;
+        ll_Node<K,V>* right_node = find(key, &prev_node);
+        if(right_node == nullptr)
+            return -1;
+        if(right_node -> key == key && value == -1){
+            right_node -> value == -1;
+        }
+        else
+        {
+            if(value == -1)
+                return 0;
+            ll_Node<K,V>* new_node = new ll_Node<K,V>(key,value);
+            new_node -> next.store(right_node);
+            if(prev_node -> next.compare_exchange_strong(right_node, new_node)) {
+                count++;
+                return 1;
+            }
+        }
+    }
+}
+
+
+
+
+
+#endif //UNTITLED_LF_LL_H
---
+++ Kanva_impl/kanva_model.h
@@ -0,0 +1,61 @@
+#ifndef KANVA_KANVA_MODEL_H
+#define KANVA_KANVA_MODEL_H
+#include "lr_model.h"
+#include "lr_model_impl.h"
+#include "util.h"
+#include "Bin_LL/Bin.h"
+
+template<class key_t, class val_t>
+class KanvaModel {
+public:
+    typedef LinearRegressionModel<key_t> lrmodel_type;
+    typedef KanvaModel<key_t, val_t> kanva_model_type;
+
+    typedef struct model_or_bin {
+        typedef union pointer{
+            kanva_model_type * ai;
+            Bin<key_t ,val_t>* lflb;
+        }pointer_t;
+        pointer_t mob;
+        bool volatile isbin = true;   // true = lb, false = ai
+        volatile uint8_t locked = 0;
+    }model_or_bin_t;
+
+public:
+    inline KanvaModel();
+    ~KanvaModel();
+    KanvaModel(lrmodel_type &lrmodel, const typename std::vector<key_t>::const_iterator &keys_begin,
+               const typename std::vector<val_t>::const_iterator &vals_begin,
+               size_t size, size_t _maxErr);
+    inline void print_model();
+    void print_keys();
+    void print_model_retrain();
+    void self_check();
+    void self_check_retrain();
+
+    val_t find_retrain(const key_t &key, val_t &val);
+    bool insert_retrain(const key_t &key, const val_t &val);
+    bool remove(const key_t &key);
+
+private:
+    inline size_t predict(const key_t &key);
+    inline size_t locate_in_levelbin(const key_t &key, const size_t pos);
+    inline size_t find_lower(const key_t &key, const size_t pos);
+    inline size_t linear_search(const key_t *arr, int n, key_t key);
+    inline size_t find_lower_avx(const int *arr, int n, int key);
+    inline size_t find_lower_avx(const int64_t *arr, int n, int64_t key);
+    bool insert_model_or_bin(const key_t &key, const val_t &val, size_t bin_pos);
+    bool remove_model_or_bin(const key_t &key, const int bin_pos);
+
+
+private:
+    lrmodel_type* model = nullptr;
+    size_t maxErr = 64;
+    size_t err = 0;
+    key_t* keys = nullptr;
+    val_t* vals = nullptr;
+    bool* valid_flag = nullptr;
+    std::atomic<model_or_bin_t*>* mobs_lf = nullptr;
+    const size_t capacity;
+};
+#endif //KANVA_KANVA_MODEL_H
